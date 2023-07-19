47b54c3818b8da73324348e42f6901dbaa4f2d78
mariana-nova <119463538+mariana-nova@users.noreply.github.com>
2023-07-18 23:24:23 -0500

    nuevas correcciones

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 import './Statistics.css'
 
 function Statistics({title, stats}, Children) {
-   
+    //Fujncion que devuelve una lista de licon datos
     return (
         <section className='statistics'>
             <h3 className='title'>{title}</h3>
 
             <ul className='statsList'>
                 {stats.map( (item) => (
-                    <li className='item'>
+                    <li key={item.id} className='item'>
                         <span className="label"> {item.label} </span>
                         <span className="percentage"> {item.percentage} </span>
                     </li>
